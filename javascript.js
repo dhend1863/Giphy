1e00bf2e16df4e178f464c2dfab1853e137c5948
David Henderson <dhend1863@gmail.com>
2019-10-11 19:16:40 -0500

    Giphy API Added

---
+++ javascript.js
@@ -0,0 +1,34 @@
+$(function () {
+    populateButtons(searchArray, 'searchButton', '#buttonsArea');
+    console.log("Page Loaded");
+})
+
+var searchArray = ['Will Ferrell', 'Chris Farley', 'Dave Chappelle'];
+
+function populateButtons(searchArray, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
+    for (var i = 0; i < searchArray.length; i++) {
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('data-type', searchArray[i])
+        a.text(searchArray[i]);
+        $(areaToAddTo).append(a);
+    }
+}
+
+$(document).on('click','.searchButton',function(){
+    var type = $(this).data('type');
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=WgNLGvTrbhuKKsNMY5e4qj7jG7LxN1EY&limit=8';
+    $.ajax({url:queryURL,method:'GET'})
+    .done(function(response){
+        for(var i=0; i<response.data.length;i++){
+            var searchDiv = $('<div class="search-item>');
+            var rating = response.data[i].rating;
+            var p = $('<p>').text('Rating: '+rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $('<img>');
+            
+        }
+    })
+})
