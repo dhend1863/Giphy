7b01191d3bc7dd04bfb487f58b0ca38f61357d6c
David Henderson <dhend1863@gmail.com>
2019-10-13 16:56:48 -0500

    Logic Complete CSS Needs to be added

--- javascript.js
+++ javascript.js
@@ -17,18 +17,47 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
 }
 
 $(document).on('click','.searchButton',function(){
+    $('#searches').empty();
     var type = $(this).data('type');
     var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=WgNLGvTrbhuKKsNMY5e4qj7jG7LxN1EY&limit=8';
     $.ajax({url:queryURL,method:'GET'})
     .done(function(response){
         for(var i=0; i<response.data.length;i++){
-            var searchDiv = $('<div class="search-item>');
+            var searchDiv = $('<div class="search-item">');
             var rating = response.data[i].rating;
             var p = $('<p>').text('Rating: '+rating);
             var animated = response.data[i].images.fixed_height.url;
             var still = response.data[i].images.fixed_height_still.url;
             var image = $('<img>');
+            image.attr('src',still);
+            image.attr('data-still',still);
+            image.attr('data-animated',animated);
+            image.attr('data-state','still');
+            image.addClass('searchImage');
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $('#searches').append(searchDiv);
+
             
         }
     })
+})
+
+$(document).on('click','.searchImage', function(){
+    var state = $(this).attr('data-state');
+    if(state === 'still'){
+        $(this).attr('src',$(this).data('animated'));
+        $(this).attr('data-state','animated');
+    } else {
+        $(this).attr('src',$(this).data('still'));
+        $(this).attr('data-state','still');
+    }
+})
+
+$('#addSearch').on('click', function(){
+    var newSearch = $('input').eq(0).val();
+    searchArray.push(newSearch);
+    populateButtons(searchArray,'searchButton', '#buttonsArea');
+    return false;
+
 })
