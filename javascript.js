3fecb6b8155a5b4c9d783acc1d36bc8b4d70845d
David Henderson <dhend1863@gmail.com>
2019-10-28 16:40:33 -0500

    Test

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
 $(document).on('click', '.searchButton', function () {
     $('#searches').empty();
     var type = $(this).data('type');
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=WgNLGvTrbhuKKsNMY5e4qj7jG7LxN1EY&limit=10';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=WgNLGvTrbhuKKsNMY5e4qj7jG7LxN1EY&limit=10';
     console.log(queryURL);
     $.ajax({ url: queryURL, method: 'GET' })
         .done(function (response) {
